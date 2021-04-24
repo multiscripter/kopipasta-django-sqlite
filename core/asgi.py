8cc9730101bb2bf5d1303e5b8a0b422b591a9d62
multiscripter <ILL-JAH@yandex.ru>
2021-04-24 17:12:55 +0300

    v12. asgi settings.

--- core/asgi.py
+++ core/asgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
-import os
+import os, sys
 
-from django.core.asgi import get_asgi_application
+# Эти пути необходимы для работы на бое в режиме mod_wsgi как демона.
+# Решают проблему ModuleNotFoundError: No module named 'имя_модуля'
+sys.path.append('/home/cyberbotx/.local/lib/python3.8/site-packages')
+sys.path.append('/var/www/kopipasta-django-sqlite')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+from django.core.asgi import get_asgi_application
+
+kopipasta_app = get_asgi_application()
